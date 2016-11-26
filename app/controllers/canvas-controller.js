77111f7e5d9ac869c4e632916370771796988b84
mike.lima <mike.lima@tegra.me>
2016-11-26 03:07:00 -0200

    Add icon

--- app/controllers/canvas-controller.js
+++ app/controllers/canvas-controller.js
@@ -5,20 +5,20 @@ var Canvas = require('../models/canvas');
 
 router.get('/:code', function (req, res) {
 
-    // Canvas.create({
-    //     code: '123',
-    //     data: {
-    //         key_partners: 'key_partners',
-    //         key_activities: 'key_activities',
-    //         key_resources: 'key_resources',
-    //         cost_structure: 'cost_structure',
-    //         value_propositions: 'value_propositions',
-    //         customer_relationships: 'customer_relationships',
-    //         channels: 'channels',
-    //         customer_segments: 'customer_segments',
-    //         revenue_streams: 'revenue_streams'
-    //     }
-    // });
+        // Canvas.create({
+        //     code: '123',
+        //     data: {
+        //         key_partners: 'key_partners',
+        //         key_activities: 'key_activities',
+        //         key_resources: 'key_resources',
+        //         cost_structure: 'cost_structure',
+        //         value_propositions: 'value_propositions',
+        //         customer_relationships: 'customer_relationships',
+        //         channels: 'channels',
+        //         customer_segments: 'customer_segments',
+        //         revenue_streams: 'revenue_streams'
+        //     }
+        // });
 
     Canvas.findOne({code: req.params.code}, function (err, canvas) {
         if (err) {
