6a239080695727c004238069b0a68e1ec8a0b333
Celso Wo <celso.wo@tegra.me>
2016-11-26 03:05:15 -0200

    Added canvas action to webhook

--- app/controllers/webhook-controller.js
+++ app/controllers/webhook-controller.js
@@ -5,29 +5,43 @@ var randomstring = require("randomstring");
 
 router.post('/', function (req, res) {
     
-    var code = randomstring.generate(7);
-    Canvas.create({
-        code: code,
-        data: {
-            key_partners: 'key_partners' + code,
-            key_activities: 'key_activities' + code,
-            key_resources: 'key_resources' + code,
-            cost_structure: 'cost_structure' + code,
-            value_propositions: 'value_propositions' + code,
-            customer_relationships: 'customer_relationships' + code,
-            channels: 'channels',
-            customer_segments: 'customer_segments' + code,
-            revenue_streams: 'revenue_streams' + code
-        }
-    });
+    if (req.body.results.action === 'canvas') {
+        
+        // Canvas.create({
+        //     code: code,
+        //     data: {
+        //         key_partners: 'key_partners' + code,
+        //         key_activities: 'key_activities' + code,
+        //         key_resources: 'key_resources' + code,
+        //         cost_structure: 'cost_structure' + code,
+        //         value_propositions: 'value_propositions' + code,
+        //         customer_relationships: 'customer_relationships' + code,
+        //         channels: 'channels',
+        //         customer_segments: 'customer_segments' + code,
+        //         revenue_streams: 'revenue_streams' + code
+        //     }
+        // });
+
+        var code = randomstring.generate(7);
+        Canvas.create({
+            code: code,
+            data: req.body.results.parameters
+        });
+
+        res.json({
+            "speech": "Montamos um modelo de negócio para você, olhe aqui.",
+            "displayText": "Montamos um modelo de negócio para você, olhe aqui.",
+            "source": "connect-api",
+            "data": {
+                url: 'https://uol-econnect.herokuapp.com/canvas/' + code
+            }
+        });
+    }
 
     res.json({
-        "speech": "Montamos um modelo de negócio para você, olhe aqui.",
-        "displayText": "Montamos um modelo de negócio para você, olhe aqui.",
+        "speech": "Não temos resposta para esta solicitação ainda, mas estamos trabalhando nisso.",
+        "displayText": "Não temos resposta para esta solicitação ainda, mas estamos trabalhando nisso.",
         "source": "connect-api",
-        "data": {
-            url: 'https://uol-econnect.herokuapp.com/canvas/' + code
-        }
     });
 }); 
 
