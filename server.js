eb5d72af2ab05d76ea206ab7a45ab3b1cec32fae
Celso Wo <celso.wo@tegra.me>
2016-11-25 22:33:38 -0200

    changed mongo db settings for production

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var port = process.env.NODE_PORT || 8080;
-var database = require('./config/database');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+if (process.env.NODE_PORT == 'production') {
+    mongoose.connect('mongodb://heorku:heroku@ds111178.mlab.com:11178/heroku_ssmwrnts');
+} else {
+    mongoose.connect('mongodb://mongo/econnect');
+}
 
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
