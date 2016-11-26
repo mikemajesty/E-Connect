f49929463ac99fea7d0e9c3013fdb4c4bdf90ee8
Celso Wo <celso.wo@tegra.me>
2016-11-26 06:35:58 -0200

    Few visual improvements

--- app/controllers/canvas-controller.js
+++ app/controllers/canvas-controller.js
@@ -29,7 +29,17 @@ router.get('/:code', function (req, res) {
             return res.status(404).send();
         }
 
-        res.render('canvas/index', canvas.data);
+        res.render('canvas/index', {
+            key_partners: canvas.data.key_partners,
+            key_activities: canvas.data.key_activities,
+            key_resources: canvas.data.key_resources,
+            cost_structure: canvas.data.cost_structure,
+            value_propositions: canvas.data.value_propositions,
+            customer_relationships: canvas.data.customer_relationships,
+            channels: canvas.data.channels,
+            customer_segments: canvas.data.customer_segments,
+            revenue_streams: canvas.data.revenue_streams
+        });
     });
 }); 
 
